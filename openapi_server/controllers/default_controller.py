22c2ae66f464da0189ae06fd1299f2690e86483e
Luis Azevedo <luis.azevedo@sabre.com>
2024-04-19 17:04:47 -0300

    controller implemented

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -6,6 +6,10 @@ from typing import Union
 from openapi_server.models.sentiment_analysis import SentimentAnalysis  # noqa: E501
 from openapi_server import util
 
+# Import sentiment analysis models
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 
 def sentiment_analysis(sentence=None):  # noqa: E501
     """Given a sentence it analyses whether the sentiment is positive or negative
@@ -17,4 +21,18 @@ def sentiment_analysis(sentence=None):  # noqa: E501
 
     :rtype: Union[SentimentAnalysis, Tuple[SentimentAnalysis, int], Tuple[SentimentAnalysis, int, Dict[str, str]]
     """
-    return 'do some magic!'
+    # Analyse the  supplied query
+    sentiment_computed =''
+    nltk.download("vader_lexicon")
+    sid = SentimentIntensityAnalyzer()
+    score = sid.polarity_scores(sentence)["compound"]
+    if score > 0:
+        sentiment_computed = 'positive' # if score is greater than zero return positive
+    else:
+        sentiment_computed =  'negative' # if score is greater than zero return negative     
+
+    senti = SentimentAnalysis(
+        sentiment = sentiment_computed
+    )
+    return senti
+
